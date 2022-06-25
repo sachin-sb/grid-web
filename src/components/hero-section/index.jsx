235107ffef15a06e9d73691df717bd1532ce694b
Sachin Sharma <sachin@gryt.fit>
2022-06-25 10:48:05 +0530

    FIX: hero section modal

--- src/components/hero-section/index.jsx
+++ src/components/hero-section/index.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import AnimatedText from "../../common/animatedText";
-import ModalVideo from "react-modal-video";
 import "react-modal-video/css/modal-video.css";
 import Split from "../Split";
 import fadeWhenScroll from "../../common/fadeWhenScroll";
@@ -9,6 +8,7 @@ import removeSlashFromPagination from "../../common/removeSlashFromPagination";
 
 
 const HeroSection = (props) => {
+
   const [load, setLoad] = React.useState(true);
   React.useEffect(() => {
     fadeWhenScroll();
@@ -43,86 +43,106 @@ const HeroSection = (props) => {
     }
   };
 
-  const [isOpen, setOpen] = React.useState(false);
+  const handleClick = () => {
+    const div = document.getElementById('modal-container');
+    div.classList.add("one");
+  }
+
+  const closeClick = () => {
+    const div = document.getElementById('modal-container');
+    div.classList.add("out");
+
+    const timer = setTimeout(() => {
+      const div = document.getElementById('modal-container');
+      div.classList.remove("one");
+      div.classList.remove("out");
+    }, 1000);
+    return () => clearTimeout(timer);
+  }
 
   return (
     <section className="slider slider-prlx home-hero-section" id={props.id}>
-      <div className="swiper-container parallax-slider">
-        {!load ? (
-          <div className="container-fluid">
-            <div className="row align-items-center">
-              <div className="col-sm-6">
-                <div className="content-sec">
-                  <Split>
-                    <p className="wow txt words chars splitting text-1" data-splitting>
-                      Crafting experiences through
-                    </p>
-                  </Split>
-                  <motion.div
-                    initial="hidden"
-                    animate={replay ? "visible" : "hidden"}
-                    variants={container}
-                    className="heading-sec"
-                  >
-                    <div className="cursor-scale">
-                      {headingText.map((item, index) => {
-                        return <AnimatedText {...item} key={index} />;
-                      })}
-                    </div>
-                  </motion.div>
+      {!load ? (
+        <div className="container-fluid">
+          <div className="row align-items-center">
+            <div className="col-sm-6">
+              <div className="content-sec">
+                <Split>
+                  <p className="wow txt words chars splitting text-1" data-splitting>
+                    Crafting experiences through
+                  </p>
+                </Split>
+                <motion.div
+                  initial="hidden"
+                  animate={replay ? "visible" : "hidden"}
+                  variants={container}
+                  className="heading-sec"
+                >
+                  <div className="cursor-scale">
+                    {headingText.map((item, index) => {
+                      return <AnimatedText {...item} key={index} />;
+                    })}
+                  </div>
+                </motion.div>
 
-                  <Split>
-                    <p className="wow txt words chars splitting text-move-style" data-splitting>
-                      that move businesses <br /> & people
-                    </p>
-                  </Split>
+                <Split>
+                  <p className="wow txt words chars splitting text-move-style" data-splitting>
+                    that move businesses <br /> & people
+                  </p>
+                </Split>
 
-                  <motion.div
-                    initial="hidden"
-                    animate={replay ? "visible" : "hidden"}
-                    variants={container}
-                    className="sub-heading"
-                  >
-                    <div className="cursor-scale">
-                      {QuoteText.map((item, index) => {
-                        return <AnimatedText {...item} key={index} />;
-                      })}
-                    </div>
-                  </motion.div>
-                </div>
+                <motion.div
+                  initial="hidden"
+                  animate={replay ? "visible" : "hidden"}
+                  variants={container}
+                  className="sub-heading"
+                >
+                  <div className="cursor-scale">
+                    {QuoteText.map((item, index) => {
+                      return <AnimatedText {...item} key={index} />;
+                    })}
+                  </div>
+                </motion.div>
               </div>
+            </div>
 
-              <div className="col-sm-6">
-                <div className="slider-section">
-                  <video loop className="video"
-                    poster="https://carleton.ca/panl/wp-content/uploads/Photo-of-mural-in-Montreal-is-courtesy-of-Mr-TT-and-Unsplash-1600.jpg"
-                    onMouseOver={event => event.target.play()}
-                    onMouseOut={event => event.target.pause()}>
-                    <source type="video/mp4" src="video/project-fi.mp4" />
-                  </video>
+            <div className="col-sm-6">
+              <div className="slider-section">
+                <video loop className="video"
+                  poster="https://carleton.ca/panl/wp-content/uploads/Photo-of-mural-in-Montreal-is-courtesy-of-Mr-TT-and-Unsplash-1600.jpg"
+                  onMouseOver={event => event.target.play()}
+                  onMouseOut={event => event.target.pause()}
+                  onClick={handleClick}
+                >
+                  <source type="video/mp4" src="video/project-fi.mp4" />
+                </video>
 
-                  <div className="button-box">
-                    <button className="btn-showreel" onClick={(e) => { e.preventDefault(); setOpen(true); }}>
-                      <img src="img/play.svg" className="img-icon" alt="" />
-                     <span className="wow fadeInLeft" data-wow-delay=".4s"> Play Showreel</span>
+                <div className="button-box">
+                  <button className="btn-showreel"
+                    onClick={handleClick}
+                  >
+                    <img src="img/play.svg" className="img-icon" alt="" />
+                    <span className="wow fadeInLeft" data-wow-delay=".4s"> Play Showreel</span>
+                  </button>
+                </div>
+
+                <div id="modal-container">
+                  <div className="modal-background">
+                    <div className="modal">
+                      <video loop autoPlay muted className="video-style">
+                        <source type="video/mp4" src="video/project-fi.mp4" />
+                      </video>
+                    </div>
+                    <button onClick={closeClick} className="modal-close-button">
+                      <img src="/img/close.svg" className="img-icon" alt="" />
                     </button>
                   </div>
-
-                  <ModalVideo
-                    channel="vimeo"
-                    autoplay
-                    isOpen={isOpen}
-                    videoId="127203262"
-                    allowFullScreen
-                    // classNames="modal-hero-section"
-                    onClose={() => setOpen(false)}
-                  />
                 </div>
               </div>
             </div>
           </div>
-        ) : null}
-      </div>
+        </div>
+      ) : null}
     </section>
   );
 };
