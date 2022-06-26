c38a67526cb53ac558c42e4b87d5f94dbd6c0790
Sachin Sharma <sachin@gryt.fit>
2022-06-27 00:56:55 +0530

    FIX: url path title and description

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Avo onepage themeforest"
+            content="Creative agency based in Gurgaon, India."
           />
           <meta
             name="description"
-            content="Avo - Onepage Multi-Purpose HTML5 Template"
+            content="Grid - Creative agency based in Gurgaon, India."
           />
           <meta name="author" content="" />
           <link rel="shortcut icon" href="/img/favicon.ico" />
