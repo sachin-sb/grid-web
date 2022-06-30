2554a2d5b84b63642f76354266086a9226014632
Sachin Sharma <sachin@gryt.fit>
2022-06-30 14:28:07 +0530

    FIX: some button issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,45 +51,45 @@ export default function Home(props) {
   });
 
   
-  // useEffect(() => {
-  //   (function () {
-  //     var indicator = $("#indicator");
-  //     var counter = indicator.find("span");
+  useEffect(() => {
+    (function () {
+      var indicator = $("#indicator");
+      var counter = indicator.find("span");
 
-  //     var win = jQuery(window);
-  //     if (indicator.length) {
-  //       var moveIndicator = debounce(function () {
-  //         var viewportHeight = $(window).height();
-  //         var documentHeight = $(document).height();
-  //         var hasScrolled = $(window).scrollTop();
+      var win = jQuery(window);
+      if (indicator.length) {
+        var moveIndicator = debounce(function () {
+          var viewportHeight = $(window).height();
+          var documentHeight = $(document).height();
+          var hasScrolled = $(window).scrollTop();
 
-  //         var percent = (hasScrolled / (documentHeight - viewportHeight)) * 100;
-  //         indicator.css("top", percent + "%");
-  //         counter.html(Math.floor(percent) + "%");
-  //       }, 10);
-  //     }
+          var percent = (hasScrolled / (documentHeight - viewportHeight)) * 100;
+          indicator.css("top", percent + "%");
+          counter.html(Math.floor(percent) + "%");
+        }, 10);
+      }
 
-  //     win.on("resize scroll", moveIndicator);
+      win.on("resize scroll", moveIndicator);
 
-  //     function debounce(func, wait, immediate) {
-  //       var timeout;
+      function debounce(func, wait, immediate) {
+        var timeout;
 
-  //       return function () {
-  //         var context = this,
-  //           args = arguments;
-  //         var later = function () {
-  //           timeout = null;
-  //           if (!immediate) func.apply(context, args);
-  //         };
-  //         var callNow = immediate && !timeout;
-  //         clearTimeout(timeout);
-  //         timeout = setTimeout(later, wait);
-  //         if (callNow) func.apply(context, args);
-  //       };
-  //     }
-  //   })();
+        return function () {
+          var context = this,
+            args = arguments;
+          var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+          };
+          var callNow = immediate && !timeout;
+          clearTimeout(timeout);
+          timeout = setTimeout(later, wait);
+          if (callNow) func.apply(context, args);
+        };
+      }
+    })();
 
-  // }, [])
+  }, [])
 
 
   return (
